a18bc5f3bbb0e222a4319bc3acdda79328727de6
appledix <dxl2010@mail.ru>
2017-01-22 14:36:50 +0300

    Первоначальный вариант.

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Night Owls Detector
+Данный скрипт задейстует API образовательного ресурса [DEVMAN.org](https://devman.org) для сбора информации об отправленных на проверку задачах и выводит в терминал список имён пользователей, отправлявших задачи на проверку в промежутке с 24:00 до 6:00 своего локального времени. [Используемый API](https://devman.org/api/challenges/solution_attempts/).
 
-[TODO. There will be project description]
+## Использование
+В терминале:
+`python3.5 seek_dev_nighters.py`
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,53 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import json
+from datetime import datetime, time
 
-def get_midnighters():
-    pass
+import pytz
+import requests
+
+SOLUTION_ATTEMPTS_INFO_URL = 'https://devman.org/api/challenges/solution_attempts/'
+OWL_PERIOD = {'start':time(0,0), 'end':time(6,0)}
+
+
+def get_number_of_pages(url):
+    return requests.get(url).json()['number_of_pages']                 
+
+def load_pages(url):
+    num_of_pages = get_number_of_pages(url)
+    for page_num in range(1, num_of_pages + 1):
+        yield requests.get(url, params={'page':page_num}).json()
+
+def get_time_of_sending(timestamp, timezone):
+    utc_datetime = datetime.utcfromtimestamp(timestamp).replace(tzinfo=pytz.utc)
+    return utc_datetime.astimezone(pytz.timezone(timezone)).time()
+
+def is_time_in_owl_period(sending_time, owl_period):
+    return sending_time >= owl_period['start'] and sending_time <= owl_period['end']
+
+def get_all_attempts(url):
+    for page in load_pages(url):
+            for attempt in page['records']:
+                yield attempt
+
+def get_midnighters(attempts, owl_period):
+    midnighters = set()
+    for attempt in attempts:
+        if attempt['timestamp']:
+            sending_time = get_time_of_sending(attempt['timestamp'],
+                                               attempt['timezone'])
+            if is_time_in_owl_period(sending_time, owl_period):
+                midnighters.add(attempt['username'])
+    return midnighters     
+
+
+def main(): 
+    attempts = get_all_attempts(SOLUTION_ATTEMPTS_INFO_URL)
+    midnighters = get_midnighters(attempts, OWL_PERIOD)
+    if midnighters:
+        print('Found midnighters:')
+        for i, user in enumerate(midnighters,1):
+            print('{}) {}'.format(i, user))
+    else:
+        print('Midnighters not found.')        
 
 if __name__ == '__main__':
-  pass
+    main()
